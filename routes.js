2bf78a2eecc29f9278cab9a148fad5d85d707ecb
jsteinfink914 <jsteinfink914@gmail.com>
2025-03-08 12:16:40 -0500

    Splitting Pages

--- routes.js
+++ routes.js
@@ -3,8 +3,8 @@ import RenterPortal from "./src/routes/RenterPortal.svelte";
 import MovingServices from "./src/routes/MovingServices.svelte";
 import PaymentPage from "./src/routes/PaymentPage.svelte";
 import LeasingDashboard from "./src/routes/LeasingDashboard.svelte";
-import SearchPage from "./routes/SearchPage.svelte";
-import ComparePage from "./routes/ComparePage.svelte";
+import SearchPage from "./src/routes/SearchPage.svelte";
+import ComparePage from "./src/routes/ComparePage.svelte";
 
 
 
